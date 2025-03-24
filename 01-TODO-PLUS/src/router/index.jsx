139cb12dd04d878c2e39e55af41b330c4e501a6e
Kerenza13 <123767067+Kerenza13@users.noreply.github.com>
2025-03-24 10:28:58 +0100

    Update index.jsx

--- 01-TODO-PLUS/src/router/index.jsx
+++ 01-TODO-PLUS/src/router/index.jsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import RootLayout from "../layouts/RootLayout";
-import TaskPage from "../pages/TaskPage";
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/HomePage";
+import CreateTaskPage from "./pages/CreateTaskPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      {
-        index: true,
-        element: <TaskPage />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "create", element: <CreateTaskPage /> },
     ],
   },
 ]);
+
