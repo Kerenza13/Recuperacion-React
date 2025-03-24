519524f4dbacb06fc136de1ad2348fd2ae510697
Kerenza13 <123767067+Kerenza13@users.noreply.github.com>
2025-03-24 10:29:39 +0100

    Update RootLayout.jsx

--- 01-TODO-PLUS/src/layouts/RootLayout.jsx
+++ 01-TODO-PLUS/src/layouts/RootLayout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <div>
+      <Outlet />
+    </div>
   );
 };
 
